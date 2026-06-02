ff64ea7d6c07730a7eb9a167efd4304bd5782217
Peerawat (kaopan) <atkaew@gmail.com>
2026-06-02 17:01:25 +0700

    feat(exercises): complete primitive values and string exercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalCost = 27000;
+let discount = (25 / 100) * totalCost;
+let netPrice = totalCost - discount;
+
+console.log(netPrice); // Output: 20250
